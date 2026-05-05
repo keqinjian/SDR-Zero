1c8ab21441e03026f0bf70ea86753d3141e612f2
keqinjian <2117157017@qq.com>
2026-05-05 12:03:56 +0800

    added jamming mock system

--- tx_radio.py
+++ tx_radio.py
@@ -73,6 +73,8 @@ class tx_radio(gr.top_block, Qt.QWidget):
         ##################################################
         # Variables
         ##################################################
+        self.target_sens = target_sens = 2.8323
+        self.target_freq = target_freq = 434920000
         self.samp_rate = samp_rate = 1000000
 
         ##################################################
@@ -118,25 +120,25 @@ class tx_radio(gr.top_block, Qt.QWidget):
 
         self._qtgui_freq_sink_x_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_grid_layout.addWidget(self._qtgui_freq_sink_x_0_win)
-        self.iio_pluto_source_0 = iio.pluto_source('ip:192.168.2.1', 433920000, 1000000, 20000000, 32768, True, True, True, 'manual', 20, '', True)
-        self.iio_pluto_sink_0 = iio.pluto_sink('ip:192.168.2.1', 433920000, 1000000, 20000000, 32768, False, 20.0, '', True)
+        self.iio_pluto_source_0 = iio.pluto_source('ip:192.168.2.1', target_freq, 1000000, 1000000, 32768, True, True, True, 'manual', 20, '', True)
+        self.iio_pluto_sink_0 = iio.pluto_sink('ip:192.168.2.1', target_freq, 1000000, 1000000, 32768, False, 10, '', True)
         self.digital_gfsk_mod_0 = digital.gfsk_mod(
             samples_per_symbol=52,
-            sensitivity=1.5756,
+            sensitivity=target_sens,
             bt=0.35,
             verbose=False,
             log=False)
         self.digital_gfsk_demod_0 = digital.gfsk_demod(
             samples_per_symbol=52,
-            sensitivity=1.5756,
+            sensitivity=target_sens,
             gain_mu=0.3,
             mu=0.5,
             omega_relative_limit=0.005,
             freq_error=0.0,
             verbose=False,
             log=False)
-        self.blocks_udp_source_0 = blocks.udp_source(gr.sizeof_char*1, '127.0.0.1', 12345, 1472, True)
-        self.blocks_udp_sink_0 = blocks.udp_sink(gr.sizeof_char*1, '127.0.0.1', 14346, 1472, True)
+        self.blocks_udp_source_0 = blocks.udp_source(gr.sizeof_char*1, '127.0.0.1', 12347, 1472, True)
+        self.blocks_udp_sink_0 = blocks.udp_sink(gr.sizeof_char*1, '127.0.0.1', 14348, 1472, True)
         self.analog_simple_squelch_cc_0 = analog.simple_squelch_cc(-50, 0.001)
 
 
@@ -155,6 +157,20 @@ class tx_radio(gr.top_block, Qt.QWidget):
         self.settings = Qt.QSettings("GNU Radio", "tx_radio")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
+
+    def get_target_sens(self):
+        return self.target_sens
+
+    def set_target_sens(self, target_sens):
+        self.target_sens = target_sens
+
+    def get_target_freq(self):
+        return self.target_freq
+
+    def set_target_freq(self, target_freq):
+        self.target_freq = target_freq
+        self.iio_pluto_sink_0.set_params(self.target_freq, 1000000, 1000000, 10, '', True)
+        self.iio_pluto_source_0.set_params(self.target_freq, 1000000, 1000000, True, True, True, 'manual', 20, '', True)
 
     def get_samp_rate(self):
         return self.samp_rate
